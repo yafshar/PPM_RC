159db74673a5db756c4172f8f4ce5bcc7a9cc3bb
Yaser <afshar@mpi-cbg.de>
2016-04-12 18:00:22 +0200

    Debugging the unique routine to avoid extra memory allocation

--- src/util/ppm_rc_unique.f
+++ src/util/ppm_rc_unique.f
@@ -1,3 +1,11 @@
+      !!! [NOTE]
+      !!! These routines are not general, they are provided for the especial
+      !!! purpose to work on the input array of labels
+      !!!
+      !!! The unique routines here are for the purpose of unique array of
+      !!! Labels which can be in the form of single or double or integer or
+      !!! long integer
+
       SUBROUTINE ppm_rc_uniquers(a,b,info)
 
         USE ppm_module_util_qsort, ONLY : ppm_util_qsort
@@ -12,13 +20,12 @@
         REAL(ppm_kind_double) :: t0
 
         INTEGER, DIMENSION(SIZE(a))    :: c
+        INTEGER, DIMENSION(SIZE(a))    :: MASKc
         INTEGER, DIMENSION(:), POINTER :: d
         INTEGER, DIMENSION(1)          :: n
         INTEGER                        :: i,j,k,iopt
 
         CHARACTER(LEN=ppm_char) :: caller ='unique'
-
-        LOGICAL, DIMENSION(SIZE(a)) :: MASK
 
         CALL substart(caller,t0,info)
 
@@ -30,14 +37,12 @@
         CALL ppm_util_qsort(c,d,info,k)
         or_fail("ppm_util_qsort")
 
-        MASK(d(1))=.TRUE.
         j=1
+        MASKc(1)=c(d(1))
         DO i=2,k
-           IF (c(d(i)).EQ.c(d(i-1))) THEN
-              MASK(d(i))=.FALSE.
-           ELSE
-              MASK(d(i))=.TRUE.
+           IF (c(d(i)).NE.c(d(i-1))) THEN
               j=j+1
+              MASKc(j)=c(d(i))
            ENDIF
         ENDDO
 
@@ -46,18 +51,13 @@
         or_fail_dealloc("unique d")
 
         n=j
-        !COUNT(MASK)
 
         iopt=ppm_param_alloc_grow
         CALL ppm_alloc(b,n,iopt,info)
         or_fail_alloc("unique b")
 
-        j=1
-        DO i=1,k
-           IF (MASK(i)) THEN
-              b(j)=c(i)
-              j=j+1
-           ENDIF
+        DO i=1,j
+           b(i)=MASKc(i)
         ENDDO
         !-------------------------------------------------------------------------
         !  Return
@@ -80,13 +80,12 @@
         REAL(ppm_kind_double) :: t0
 
         INTEGER, DIMENSION(SIZE(a))    :: c
+        INTEGER, DIMENSION(SIZE(a))    :: MASKc
         INTEGER, DIMENSION(:), POINTER :: d
         INTEGER, DIMENSION(1)          :: n
         INTEGER                        :: i,j,k,iopt
 
         CHARACTER(LEN=ppm_char) :: caller ='unique'
-
-        LOGICAL, DIMENSION(SIZE(a)) :: MASK
 
         CALL substart(caller,t0,info)
 
@@ -98,14 +97,12 @@
         CALL ppm_util_qsort(c,d,info,k)
         or_fail("ppm_util_qsort")
 
-        MASK(d(1))=.TRUE.
         j=1
+        MASKc(1)=c(d(1))
         DO i=2,k
-           IF (c(d(i)).EQ.c(d(i-1))) THEN
-              MASK(d(i))=.FALSE.
-           ELSE
-              MASK(d(i))=.TRUE.
+           IF (c(d(i)).NE.c(d(i-1))) THEN
               j=j+1
+              MASKc(j)=c(d(i))
            ENDIF
         ENDDO
 
@@ -113,19 +110,14 @@
         CALL ppm_alloc(d,n,iopt,info)
         or_fail_dealloc("unique d")
 
-        n = j
-        !COUNT(MASK)
+        n=j
 
         iopt=ppm_param_alloc_grow
         CALL ppm_alloc(b,n,iopt,info)
         or_fail_alloc("unique b")
 
-        j=1
-        DO i=1,k
-           IF (MASK(i)) THEN
-              b(j)=c(i)
-              j=j+1
-           ENDIF
+        DO i=1,j
+           b(i)=MASKc(i)
         ENDDO
         !-------------------------------------------------------------------------
         !  Return
@@ -147,13 +139,12 @@
 
         REAL(ppm_kind_double) :: t0
 
+        INTEGER, DIMENSION(SIZE(a))    :: MASKc
         INTEGER, DIMENSION(:), POINTER :: d
         INTEGER, DIMENSION(1)          :: n
         INTEGER                        :: i,j,k,iopt
 
         CHARACTER(LEN=ppm_char) :: caller ='unique'
-
-        LOGICAL, DIMENSION(SIZE(a)) :: MASK
 
         CALL substart(caller,t0,info)
 
@@ -164,14 +155,12 @@
         CALL ppm_util_qsort(a,d,info,k)
         or_fail("ppm_util_qsort")
 
-        MASK(d(1))=.TRUE.
         j=1
+        MASKc(1)=a(d(1))
         DO i=2,k
-           IF (a(d(i)).EQ.a(d(i-1))) THEN
-              MASK(d(i))=.FALSE.
-           ELSE
-              MASK(d(i))=.TRUE.
+           IF (a(d(i)).NE.a(d(i-1))) THEN
               j=j+1
+              MASKc(j)=a(d(i))
            ENDIF
         ENDDO
 
@@ -180,18 +169,13 @@
         or_fail_dealloc("unique d")
 
         n=j
-        !COUNT(MASK)
 
         iopt=ppm_param_alloc_grow
         CALL ppm_alloc(b,n,iopt,info)
         or_fail_alloc("unique b")
 
-        j=1
-        DO i=1,k
-           IF (MASK(i)) THEN
-              b(j)=a(i)
-              j=j+1
-           ENDIF
+        DO i=1,j
+           b(i)=MASKc(i)
         ENDDO
         !-------------------------------------------------------------------------
         !  Return
@@ -213,33 +197,28 @@
 
         REAL(ppm_kind_double) :: t0
 
-        INTEGER(ppm_kind_int64), DIMENSION(SIZE(a))    :: c
+        INTEGER(ppm_kind_int64), DIMENSION(SIZE(a))    :: MASKc
         INTEGER,                 DIMENSION(:), POINTER :: d
         INTEGER,                 DIMENSION(1)          :: n
         INTEGER                                        :: i,j,k,iopt
 
         CHARACTER(LEN=ppm_char) :: caller ='unique'
 
-        LOGICAL, DIMENSION(SIZE(a)) :: MASK
-
         CALL substart(caller,t0,info)
 
-        c=a
-        k=SIZE(c,DIM=1)
+        k=SIZE(a,DIM=1)
         IF (k.EQ.0) GOTO 9999
 
         NULLIFY(d)
-        CALL ppm_util_qsort(c,d,info,k)
+        CALL ppm_util_qsort(a,d,info,k)
         or_fail("ppm_util_qsort")
 
-        MASK(d(1))=.TRUE.
         j=1
+        MASKc(1)=a(d(1))
         DO i=2,k
-           IF (c(d(i)).EQ.c(d(i-1))) THEN
-              MASK(d(i))=.FALSE.
-           ELSE
-              MASK(d(i))=.TRUE.
+           IF (a(d(i)).NE.a(d(i-1))) THEN
               j=j+1
+              MASKc(j)=a(d(i))
            ENDIF
         ENDDO
 
@@ -248,18 +227,13 @@
         or_fail_dealloc("unique d")
 
         n=j
-        !COUNT(MASK)
 
         iopt=ppm_param_alloc_grow
         CALL ppm_alloc(b,n,iopt,info)
         or_fail_alloc("unique b")
 
-        j=1
-        DO i=1,k
-           IF (MASK(i)) THEN
-              b(j)=c(i)
-              j=j+1
-           ENDIF
+        DO i=1,j
+           b(i)=MASKc(i)
         ENDDO
         !-------------------------------------------------------------------------
         !  Return
